d986cbd58e46d919664e32c950bf4e77879e217e
Taylor Williams <taylor@thewilliams.net>
2019-01-02 20:34:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    session[:username] ||= nil
+    session[:name] ||= nil
   end
 
   helper_method :current_user
